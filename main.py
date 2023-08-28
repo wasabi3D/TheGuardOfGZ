34eeaf3c3dc0bd2ae89001cf960b156c59b1f486
wasabi3D <tryexceptfinally5@gmail.com>
2023-08-28 23:23:24 +0200

    Command experiment

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import discord
+from discord.ext import commands
 import sys
 
 
 def main():
     token = sys.argv[1]
     intents = discord.Intents.all()
-    client = discord.Client(intents=intents)
+    bot = commands.Bot(command_prefix="/", intents=intents)
+    guild_gz = discord.Object(id=687998122777116707)  # Gaming Zone Guild Object
 
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f"{client.user} has connected to Discord")
+        await bot.tree.sync(guild=guild_gz)
+        print(f"{bot.user} has connected to Discord")
 
-    @client.event
+    @bot.event
     async def on_message(message: discord.Message):
-        if message.author == client.user:
+        if message.author == bot.user:
             return
+        await bot.process_commands(message)
 
-        await message.channel.send("Hello!")
+    @bot.tree.command(name="ping", guild=guild_gz)
+    async def _ping(cxt: discord.Interaction):
+        await cxt.response.send_message("Pong!")
 
-    print(token, type(token))
-    client.run(token)
+    @bot.tree.command(name="add", guild=guild_gz)
+    async def _add(cxt: discord.Interaction, a: int, b: int):
+        await cxt.response.send_message(f"{a}+{b}={a + b}")
+
+    bot.run(token)
 
 
 if __name__ == "__main__":
